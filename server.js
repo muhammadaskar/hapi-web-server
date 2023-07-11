ba22ab817199de1e0a285bcc26bb9c86038efcd9
muhammad.askar <muhammad.askar@indocyber.id>
2023-07-12 01:31:52 +0700

    fix: edit host

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import routes from './routes.js';
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: '0.0.0.0',
     });
 
     server.route(routes)
